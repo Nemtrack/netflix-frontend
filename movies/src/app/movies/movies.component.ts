fc7da485b7a2ca7ad01c5ef31c47203ae65d8e7a
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-10-22 21:46:46 +0200

    Implement cross-component messaging of services

--- movies/src/app/movies/movies.component.ts
+++ movies/src/app/movies/movies.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FiltersService } from '../shared/filters.service';
+import { ServicesService } from '../shared/services.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css'],
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+  constructor(
+    private filters: FiltersService,
+    private servicesServ: ServicesService
+  ) {}
 
-  constructor(private filters: FiltersService) { }
+  service?: string;
+
+  ngOnInit(): void {
+    this.service = this.servicesServ.getService();
+  }
 
   items = [1, 2, 3, 4, 5, 6];
 }
