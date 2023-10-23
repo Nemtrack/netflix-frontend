a3cd840f2e71c749cc661c7a9753a115c186dfae
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-10-24 00:18:00 +0200

    Prepare app for communication with the API

--- movies/src/app/shared/data-storage.service.ts
+++ movies/src/app/shared/data-storage.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  countriesOptions = {
+    method: 'GET',
+    url: `${environment.streamingAvailabilityBaseUrl}/countries`,
+    headers: {
+      'X-RapidAPI-Key': environment.XRapidAPIKeyHeaderValue,
+      'X-RapidAPI-Host': environment.XRapidAPIHostHeaderValue,
+    },
+  };
+
+  getCountriesData() {
+    return this.http.request(
+      this.countriesOptions.method,
+      this.countriesOptions.url,
+      { headers: this.countriesOptions.headers }
+    );
+  }
 }
