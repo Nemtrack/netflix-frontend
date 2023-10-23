5dab38dc277eb52e856d26b0d346d338fff7482e
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-10-23 21:53:51 +0200

    Add routing for flags

--- movies/src/app/app-routing.module.ts
+++ movies/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
 import { MoviesComponent } from './movies/movies.component';
 import { ErrorComponent } from './error/error.component';
+import { FlagsComponent } from './flags/flags.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeScreenComponent, pathMatch: 'full' },
   { path: 'movies', component: MoviesComponent, pathMatch: 'full' },
+  { path: 'flags', component: FlagsComponent, pathMatch: 'full'},
   { path: '**', component: ErrorComponent, pathMatch: 'full' },
 ];
 
