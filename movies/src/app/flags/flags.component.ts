d930167a1ead82dbda9334d4eb07594b029563e0
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-10-24 00:32:21 +0200

    Changes in .gitignore and subscribed to data-storage API call

--- movies/src/app/flags/flags.component.ts
+++ movies/src/app/flags/flags.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-flags',
   templateUrl: './flags.component.html',
-  styleUrls: ['./flags.component.css']
+  styleUrls: ['./flags.component.css'],
 })
-export class FlagsComponent {
+export class FlagsComponent implements OnInit {
+  constructor(private http: DataStorageService) {}
 
+  ngOnInit(): void {
+    this.http.getCountriesData().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  countries = [];
 }
