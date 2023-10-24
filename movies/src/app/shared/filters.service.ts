949a57fad9de5a34d577a7831f78d0c07770bc3e
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-10-24 19:45:32 +0200

    Make country property with getter and setter on filter.service

--- movies/src/app/shared/filters.service.ts
+++ movies/src/app/shared/filters.service.ts
@@ -8,12 +8,21 @@ export class FiltersService {
   constructor() {}
 
   private service?: string;
+  private country?: string;
 
   setService(service: string) {
     this.service = service;
   }
 
+  setCountry(country: string) {
+    this.country = country;
+  }
+
   getService(): string | undefined {
     return this.service;
+  }
+
+  getCountry(): string | undefined {
+    return this.country;
   }
 }
